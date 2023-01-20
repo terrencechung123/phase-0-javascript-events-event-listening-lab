53dbfe0b67c09131c2618dfcc0351f1d9bf6bc33
Terrence Chung <terrencechung35@gmail.com>
2023-01-19 16:41:01 -0800

    lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
-}
+function addingEventListener(clickAlert) {
+//     const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+// }
+  
+  const input = document.getElementById('button');
+input.addEventListener('click', clickAlert);
+  alert('I was clicked!');
+};
